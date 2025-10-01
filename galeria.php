9ecae32525f01e7f7a5b4497f86a3af8fbbb2a98
EuggeGomez <euggegomez15@gmail.com>
2025-09-30 21:24:43 -0500

    Create galeria.php

---
+++ galeria.php
@@ -0,0 +1,107 @@
+<?php
+
+$obras_fauvistas = [
+    [
+        'titulo' => 'Armonía en Rojo',
+        'artista' => 'Henri Matisse',
+        'anio' => '1908',
+        'imagen' => 'imagenes/1armonia_en_rojo-matisse.webp'
+    ],
+    [
+        'titulo' => 'La Ventana Abierta',
+        'artista' => 'Henri Matisse',
+        'anio' => '1905',
+        'imagen' => 'imagenes/2la ventaana abierta-matisse.webp'
+    ],
+    [
+        'titulo' => 'La Alegría de Vivir',
+        'artista' => 'Henri Matisse',
+        'anio' => '1905-1906',
+        'imagen' => 'imagenes/3la_alegria_de_vivir-matisse.webp'
+    ],
+    [
+        'titulo' => 'La Danza',
+        'artista' => 'Henri Matisse',
+        'anio' => '1909-1910',
+        'imagen' => 'imagenes/4LADANZA-matisse.jpg'
+    ],
+    [
+        'titulo' => 'Mujer con Sombrero',
+        'artista' => 'Henri Matisse',
+        'anio' => '1905',
+        'imagen' => 'imagenes/6Matisse-Woman-with-a-Hat.jpg'
+    ],
+    [
+        'titulo' => 'Maqueta Noche de Navidad',
+        'artista' => 'Henri Matisse',
+        'anio' => '1952',
+        'imagen' => 'imagenes/5maqueta_noche_de_navidad-matisse.webp'
+    ],
+    [
+        'titulo' => 'La Curva del Camino',
+        'artista' => 'André Derain',
+        'anio' => '1906',
+        'imagen' => 'imagenes/7curva_del_camino-derain.webp'
+    ],
+    [
+        'titulo' => 'Mujer con Camisa',
+        'artista' => 'André Derain',
+        'anio' => '1905',
+        'imagen' => 'imagenes/8derain2-cke.webp'
+    ],
+    [
+        'titulo' => 'La Danza - Baile de la Vida',
+        'artista' => 'André Derain',
+        'anio' => '1906',
+        'imagen' => 'imagenes/9la_danza_baile_de_la_vida-derain.webp'
+    ],
+    [
+        'titulo' => 'El Puente de Charing Cross',
+        'artista' => 'André Derain',
+        'anio' => '1906',
+        'imagen' => 'imagenes/10puente_de_charing_cross-derain.jpg'
+    ],
+    [
+        'titulo' => 'Retrato de Henri Matisse',
+        'artista' => 'André Derain',
+        'anio' => '1905',
+        'imagen' => 'imagenes/11retrato_de_matisse-Derain.jpg'
+    ],
+    [
+        'titulo' => 'Retrato de André Derain',
+        'artista' => 'Maurice de Vlaminck',
+        'anio' => '1905',
+        'imagen' => 'imagenes/12portrait-of-derain-vlaminck.png'
+    ],
+    [
+        'titulo' => 'Autumn',
+        'artista' => 'Maurice de Vlaminck',
+        'anio' => '1905',
+        'imagen' => 'imagenes/13autumn-vlaminck.png'
+    ],
+    [
+        'titulo' => 'Restaurant de la Machine Bougival',
+        'artista' => 'Maurice de Vlaminck',
+        'anio' => '1905',
+        'imagen' => 'imagenes/14restaurant_de_la_machine_bougival-vla.png'
+    ],
+    [
+        'titulo' => 'The Orchard',
+        'artista' => 'Maurice de Vlaminck',
+        'anio' => '1906',
+        'imagen' => 'imagenes/15maurice-the-orchard-cke.webp'
+    ],
+    [
+        'titulo' => 'Chaville',
+        'artista' => 'Maurice de Vlaminck',
+        'anio' => '1906',
+        'imagen' => 'imagenes/16chaville-vlaminck.webp'
+    ],
+    [
+        'titulo' => 'Veleros de Papel',
+        'artista' => 'Maurice de Vlaminck',
+        'anio' => '1905',
+        'imagen' => 'imagenes/17veleros_de_papel-vlaminck.webp'
+    ],
+];
+?>
